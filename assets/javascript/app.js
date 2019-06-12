89c31cb09f9fb0d627238b1bab6b8ce4349b7c3b
Whittle <matt.whittle10@gmail.com>
2019-06-12 00:27:35 -0600

    added DOM manipulation for questions and answers

--- assets/javascript/app.js
+++ assets/javascript/app.js
@@ -2,74 +2,97 @@ $(document).ready(function() {
     var triviaQuestions = [
         {
             question: "What is Michael Scott's middle name?",
-            choice: ["George", "Stephen", "Gary", "Jay"],
+            choice: [" George ", " Stephen ", " Gary ", " Jay "],
             answer: 2,
             // photo: ,
         },
         {
             question: "Where did Michael get his 'World's Best Boss Mug?'",
-            choice: ["Over the Moon", "Spencers", "Heaven & Earth Gift Shop", "La Ti Da"],
+            choice: [" Over the Moon ", " Spencers ", " Heaven & Earth Gift Shop ", " La Ti Da "],
             answer: 1,
             // photo: ,
         },
         {
             question: "What is Dwight Schrute's middle name?",
-            choice: ["Kyle", "John", "Kurt", "Luke" ],
+            choice: [" Kyle ", " John ", " Kurt ", " Luke " ],
             answer: 2,
             // photo: ,
         },
         {
             question: "What does Kevin wear to Jim and Pam's wedding for shoes?",
-            choice: ["Slippers", "Tissue Boxes", "Garbage Bags", "Just his Socks"],
+            choice: [" Slippers ", " Tissue Boxes ", " Garbage Bags ", " Just his Socks "],
             answer: 1,
             // photo: ,
         },
         {
-            question: "What is Darryl doing before the 'Fun Run' during Michae's speech?",
-            choice: ["Feeding a Squirrel", "Sleeping", "Eating Potato Chips", "Driving the Forklift"],
+            question: "What is Darryl doing before the 'Fun Run' during Michael's speech?",
+            choice: [" Feeding a Squirrel ", " Sleeping ", " Eating Potato Chips ", " Driving the Forklift "],
             answer: 0,
             // photo: ,
         },
         {
             question: "What type of chips did Jim and Karen search for all day?",
-            choice: ["Herr's", "Lays", "Terra", "Kettle" ],
+            choice: [" Herr's ", " Lays ", " Terra ", " Kettle " ],
             answer: 0,
             // photo: ,
         },
         {
             question: "Jim left Scranton to go where?",
-            choice: ["Dalton", "Parker", "Utica", "Stamford"],
+            choice: [" Dalton ", " Parker ", " Utica ", " Stamford "],
             answer: 3,
             // photo: ,
         },
         {
             question: "Who cuts of the top of the Christmas Tree during the Christmas Party?",
-            choice: ["Creed", "Michael", "Kevin", "Jim"],
+            choice: [" Creed ", " Michael ", " Kevin ", " Jim "],
             answer: 2,
             // photo: ,
         },
         {
             question: "What is Andy Bernard's middle name?",
-            choice: ["Baines", "Michael", "Birch", "Judd"],
+            choice: [" Baines ", " Michael ", " Birch ", " Judd "],
             answer: 0,
             // photo: ,
         },
         {
             question: "Who plays the part of 'Golden Face' in Threat Level Midnight?",
-            choice: ["Michael", "Dwight"],
+            choice: [" Michael ", " Dwight ", " Jim ", " Toby "],
             answer: 1,
             // photo: ,
         }];
     var currentQuestion;
+    // var unansweredQuestions = 0;
     var select;
     var countDown = false; 
-    var timer = 30;
+    var timer = 15;
+    var answerArray = [];
     
     $("#restart").hide();
     $("#start").click(function() {
         $("#start").hide();
-        showQuestion();
         startTimer();
+        for (var i=0; i < triviaQuestions.length; i++) {
+            answerArray.push(triviaQuestions[i]);
+            console.log(answerArray)
+        //function to randomly generate a question from the array
+        function showQuestion() {
+            currentQuestion = Math.floor(Math.random() * triviaQuestions.length);
+            console.log(currentQuestion);
+            //variable to get the objects out of the index chosen in the array
+            select = triviaQuestions[currentQuestion];
+            console.log(select);
+            
+            $("#question").html("<h3> Question: " + select.question + "</h3>");
+            for(var i=0; i < select.choice.length; i++) {
+                var usersChoice = $("<div>");
+                usersChoice.addClass("usersChoice");
+                usersChoice.html(select.choice[i]);
+                usersChoice.attr("guess", i);
+                $("#options").html(select.choice);
+            }
+        }showQuestion()
+    }
+
     })
 
     //timer start
@@ -80,21 +103,24 @@ $(document).ready(function() {
         }
     }
 
+    //function to write the timer to the DOM 
     function decrease() {
         $("#timer").html("<h3> Time Remaining " + timer + "</h3>");
         timer --;
+        
+        if (timer === 0) {  
+                // unanswereQuestions++;
+                stopTimer();
+                $("#unanswered").html("<h5> Time is up. The correct answer is: " + select.choice[select.answer] + "<h5>")
+            }
+}
+
+    //function to stop the timer
+    function stopTimer() {
+        countDown = false;
+        clearInterval(intervalId);
     }
 
-
-    //function to randomly generate a question from the array
-    function showQuestion() {
-        currentQuestion = Math.floor(Math.random() * triviaQuestions.length);
-        console.log(currentQuestion);
-        //variable to get the objects out of the index chosen in the array
-        select = triviaQuestions[currentQuestion];
-        console.log(select);
-    }
-    showQuestion();
 });
 
 
