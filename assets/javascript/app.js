bc5f986783600b8d0c7f40815c82c0c3101bb29e
Whittle <matt.whittle10@gmail.com>
2019-06-12 23:50:02 -0600

    CSS added

--- assets/javascript/app.js
+++ assets/javascript/app.js
@@ -2,62 +2,62 @@ $(document).ready(function() {
     var triviaQuestions = [
         {
             question: "What is Michael Scott's middle name?",
-            choice: [" George ", " Stephen ", " Gary ", " Jay "],
+            choice: [" George, ", " Stephen, ", " Gary, ", " Jay, "],
             answer: 2,
             // photo: ,
         },
         {
             question: "Where did Michael get his 'World's Best Boss Mug?'",
-            choice: [" Over the Moon ", " Spencers ", " Heaven & Earth Gift Shop ", " La Ti Da "],
+            choice: [" Over the Moon, ", " Spencers, ", " Heaven & Earth Gift Shop, ", " La Ti Da, "],
             answer: 1,
             // photo: ,
         },
         {
             question: "What is Dwight Schrute's middle name?",
-            choice: [" Kyle ", " John ", " Kurt ", " Luke " ],
+            choice: [" Kyle, ", " Fart, ", " Kurt, ", " Luke, " ],
             answer: 2,
             // photo: ,
         },
         {
             question: "What does Kevin wear to Jim and Pam's wedding for shoes?",
-            choice: [" Slippers ", " Tissue Boxes ", " Garbage Bags ", " Just his Socks "],
+            choice: [" Slippers, ", " Tissue Boxes, ", " Garbage Bags, ", " Just his Socks, "],
             answer: 1,
             // photo: ,
         },
         {
             question: "What is Darryl doing before the 'Fun Run' during Michael's speech?",
-            choice: [" Feeding a Squirrel ", " Sleeping ", " Eating Potato Chips ", " Driving the Forklift "],
+            choice: [" Feeding a Squirrel, ", " Sleeping, ", " Eating Potato Chips, ", " Driving the Forklift, "],
             answer: 0,
             // photo: ,
         },
         {
             question: "What type of chips did Jim and Karen search for all day?",
-            choice: [" Herr's ", " Lays ", " Terra ", " Kettle " ],
+            choice: [" Herr's, ", " Lays, ", " Terra, ", " Kettle, " ],
             answer: 0,
             // photo: ,
         },
         {
             question: "Jim left Scranton to go where?",
-            choice: [" Dalton ", " Parker ", " Utica ", " Stamford "],
+            choice: [" Dalton, ", " Parker, ", " Utica, ", " Stamford,"],
             answer: 3,
             // photo: ,
         },
         {
             question: "Who cuts of the top of the Christmas Tree during the Christmas Party?",
-            choice: [" Creed ", " Michael ", " Kevin ", " Jim "],
+            choice: [" Creed, ", " Michael, ", " Kevin, ", " Jim, "],
             answer: 2,
             // photo: ,
         },
         {
             question: "What is Andy Bernard's middle name?",
-            choice: [" Baines ", " Michael ", " Birch ", " Judd "],
+            choice: [" Baines, ", " Michael, ", " Birch, ", " Judd, "],
             answer: 0,
             // photo: ,
         },
         {
             question: "Who plays the part of 'Golden Face' in Threat Level Midnight?",
-            choice: [" Michael ", " Dwight ", " Jim ", " Toby "],
-            answer: 1,
+            choice: [" Michael, ", " Dwight, ", " Jim, ", " Toby, "],
+            answer: 2,
             // photo: ,
         }];
     var currentQuestion;
@@ -71,23 +71,22 @@ $(document).ready(function() {
     $("#start").click(function() {
         $("#start").hide();
         startTimer();
-        for (var i=0; i < triviaQuestions.length; i++) {
+        for (var i = 0; i < triviaQuestions.length; i++) {
             answerArray.push(triviaQuestions[i]);
             console.log(answerArray)
         //function to randomly generate a question from the array
         function showQuestion() {
             currentQuestion = Math.floor(Math.random() * triviaQuestions.length);
-            console.log(currentQuestion);
+            // console.log(currentQuestion);
             //variable to get the objects out of the index chosen in the array
             select = triviaQuestions[currentQuestion];
-            console.log(select);
-            
+            // console.log(select);
             
             $("#question").html("<h3> Question: " + select.question + "</h3>");
-            for(var i=0; i < select.choice.length; i++) {
+            for(var i = 0; i < select.choice.length; i++) {
                 var usersChoice = $("<div>");
                 usersChoice.addClass("usersChoice");
-                usersChoice.html(select.choice[i]);     
+                usersChoice.html(select.choice[i]);    
                 usersChoice.attr("guess", i);
                 $("#options").html(select.choice);
             }
@@ -119,32 +118,38 @@ $(document).ready(function() {
             }
         
     $("#restart").click(function(){
-        timer = 10;
-        startTimer();
-        for (var i=0; i < triviaQuestions.length; i++) {
-            answerArray.push(triviaQuestions[i]);
-            console.log(answerArray)
+        $("#alert").hide();
+        setTimeout(function(){
+            $("#alert").show();
+        },10000);
+            timer = 10;
+            startTimer();
+            for (var i = 0; i < triviaQuestions.length; i++) {
+                answerArray.push(triviaQuestions[i]);
+                console.log(answerArray)
         //function to randomly generate a question from the array
         function showQuestion() {
             currentQuestion = Math.floor(Math.random() * triviaQuestions.length);
-            console.log(currentQuestion);
+            // console.log(currentQuestion);
             //variable to get the objects out of the index chosen in the array
             select = triviaQuestions[currentQuestion];
-            console.log(select);
+            // console.log(select);
             
             
             $("#question").html("<h3> Question: " + select.question + "</h3>");
-            for(var i=0; i < select.choice.length; i++) {
+            for(var i = 0; i < select.choice.length; i++) {
                 var usersChoice = $("<div>");
                 usersChoice.addClass("usersChoice");
                 usersChoice.html(select.choice[i]);     
                 usersChoice.attr("guess", i);
                 $("#options").html(select.choice);
             }
-        }showQuestion()
-    }
-})
         
+        }showQuestion();
+    
+    }
+    
+})      
 
 }
 
